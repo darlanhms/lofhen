bc4a19e116e9bfcb6c9ad3c4738c3aacef513b5c
darlanhms <darlanhermessjb@hotmail.com>
2023-01-28 11:29:16 -0300

    test: validate env vars before running

--- apps/api/src/core/tests/setup.ts
+++ apps/api/src/core/tests/setup.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import '@infra/prisma/middlewares';
 import '@infra/containers';
+import '../utils/env';
 import { faker } from '@faker-js/faker';
 import { config } from 'dotenv';
 
